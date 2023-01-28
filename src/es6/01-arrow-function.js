a47e4853d1cd7071547280e2ff84c7865a3cd9c8
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-01-28 08:57:51 -0500

    cambios en arrows functions

--- src/es6/01-arrow-function.js
+++ src/es6/01-arrow-function.js
@@ -1,10 +1,11 @@
-function square(num) {
+function square1(num) {
     return num * num;
 }
 
-const square = (num) => {
+const square2 = (num) => {
     return num * num;
 }
 
 const square = num => num * num;
 
+console.log(square1(5))
