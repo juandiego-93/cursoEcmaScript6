de79f6e8bcdf593654b8d602bd27a4eab434c8a2
Juan Diego S <juandiegoo_93@hotmail.com>
2023-01-28 09:39:10 -0500

    rest-spreads2

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -9,3 +9,21 @@ console.log(a, fruits[1]);
 let user = {username: 'Oscar', age: 34};
 let { username, age} = user;
 console.log(username, user.age);
+
+//spread operator 
+
+let person = {name: 'JD', age: 29};
+let country = 'PE'
+let data = {id: 1, ...person, country}; 
+
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0]
+}
+
+sum(1,1,2,3)
