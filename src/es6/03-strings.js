240f34ad38cf29d33ecb4dcdac2d53f0b9bb48c1
Juan Diego S <juandiegoo_93@hotmail.com>
2023-01-28 09:14:27 -0500

    strings 2

---
+++ src/es6/03-strings.js
@@ -0,0 +1,20 @@
+let hello = 'Hello';
+let world = 'World';
+
+let epicPhrase = hello + ' ' + world + '!';
+console.log(epicPhrase);
+
+//template literals
+
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+//multiline strings
+
+let lorem = 'esto es un strings \n' + 'esto es otra linea';
+
+let lorem2 = `Esta es una frase epica 
+la continuación de esa frase epica.`;
+
+console.log(lorem);
+console.log(lorem2);
